14de67d982c70a5acccb48d66ad6e1d00fcbfc3f
ADG 35 <adgrit35@ahmed.local>
2019-03-30 10:22:14 +0530

    Initial Commit

---
+++ Text_to_speech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Text_to_speech
+//
+//  Created by ADG RIT 35 on 30/03/19.
+//  Copyright © 2019 ADG RIT 35. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Text_to_speechTests/Text_to_speechTests.swift
@@ -0,0 +1,36 @@
+//
+//  Text_to_speechTests.swift
+//  Text_to_speechTests
+//
+//  Created by ADG RIT 35 on 30/03/19.
+//  Copyright © 2019 ADG RIT 35. All rights reserved.
+//
+
+import XCTest
+@testable import Text_to_speech
+
+class Text_to_speechTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
